d3ea919906063b95be38566b4e4de1acd57ee356
Luis Carlos <luiscarlosgallegomorales94@gmail.com>
2026-04-11 23:39:53 -0500

    Feat: Se termina la logica de los escenarios

--- src/scenarios/scenarios.controller.ts
+++ src/scenarios/scenarios.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete } from '@nestjs/common';
 import { ScenariosService } from './scenarios.service';
+import { crearEscenarioDto } from './dto/crear-escenario.dto';
+import { actualizarEscenarioDto } from './dto/actualizar.dto';
 
 @Controller('scenarios')
 export class ScenariosController {
@@ -9,4 +11,30 @@ export class ScenariosController {
     obtenerEscenarios() {
         return this.scenariosService.obtenerEscenarios();
     }
+
+    @Get(':id')
+    obtenerEscenarioPorId(@Param('id') id: string) {
+        return this.scenariosService.obtenerEscenarioPorId(Number(id));
+    }
+
+    @Post()
+    crearEscenario(@Body() crearEscenarioDto: crearEscenarioDto) {
+        return this.scenariosService.crearEscenario(crearEscenarioDto);
+  }
+
+    @Patch(':id')
+    actualizarEscenario(
+    @Param('id') id: string,
+    @Body() actualizarEscenarioDto: actualizarEscenarioDto,
+  ) {
+    return this.scenariosService.actualizarEscenario(
+      Number(id),
+      actualizarEscenarioDto,
+    );
+  }
+
+    @Delete(':id')
+    eliminarEscenario(@Param('id') id: string) {
+    return this.scenariosService.eliminarEscenario(Number(id));
+  }
 }
